cf36a2951e5e5f4059421bcdb7a328c5b3a3105e
Марсавина Диана <diii.marsavinaaa.25@gmail.com>
2026-03-10 13:31:53 +0300

    задание 1: цифровой паспорт строительного объекта

---
+++ task_01.py
@@ -0,0 +1,25 @@
+student_name = "Марсавина Диана Владимировна"
+group_number = "3140801/52501"
+project_name = 'ЖК "Новый город"'
+floors = 4
+height = 12.0
+is_residential = True
+construction_year = 2023
+print(" ПАСПОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА ")
+print("Составитель:", student_name)
+print("Группа:", group_number)
+print()
+print("Объект:", project_name)
+print("Этажность:", floors, "этажей")
+print("Высота:", height, "м")
+if is_residential:
+    building_type = "Жилой"
+else:
+    building_type = "Нежилой"
+print("Тип:", building_type)
+print("Год постройки:", construction_year)
+
+"""
+Объект находится по адресу: г. Архангельск, ул. Федота Фубина, д. 4. 
+Я выбрала этот объект, потому что для этого дома я выполняла расчет вариантов фундаментов для выпускной работы бакалавриата.
+"""
